ef9ce95e1a433288faf3d51cca0c35a1faef393d
ljc70 <38304501+ljc70@users.noreply.github.com>
2018-11-22 16:02:11 +0800

    Update config.js

--- koa2-project/db/config.js
+++ koa2-project/db/config.js
@@ -1,18 +1,9 @@
-// var config = {
-//     dialect: 'mysql',
-//     database: 'pets',
-//     username: 'root',
-//     password: 'mysql123456',
-//     host: 'localhost',
-//     port: 3306
-// };
-
 var config = {
     dialect: 'mysql',
     database: 'BLOG',
     username: 'root',
-    password: 'llc7048',
-    host: '39.107.85.167',
+    password: 'root123',
+    host: 'localhost',
     port: 3306
 };
 
